84c5a48667f0ad36dbc625a3ca10a422e40f4dbc
BrunoWM <bwillmoura3595@gmail.com>
2021-12-04 20:57:09 -0300

    Projeto concluído.

--- Caculadora/Calculadora.cs
+++ Caculadora/Calculadora.cs
@@ -4,7 +4,57 @@ using System.Text;
 
 namespace Caculadora
 {
-    class Calculadora
+    public class Calculadora
     {
+        private List<string> historico;
+        private string data;
+
+        public Calculadora(string data)
+        {
+            historico = new List<string>();
+            this.data = data;
+        }
+
+        public int Somar(int valor01, int valor02)
+        {
+            var resultado = valor01 + valor02;
+
+            historico.Insert(0, $"{data} | {valor01} + {valor02} = {resultado}");
+
+            return resultado;
+        }
+
+        public int Subtrair(int valor01, int valor02)
+        {
+            var resultado = valor01 - valor02;
+
+            historico.Insert(0, $"{data} | {valor01} - {valor02} = {resultado}");
+
+            return resultado;
+        }
+
+        public int Dividir(int valor01, int valor02)
+        {
+            var resultado = valor01 / valor02;
+
+            historico.Insert(0, $"{data} | {valor01} / {valor02} = {resultado}");
+
+            return resultado;
+        }
+
+        public int Multiplicar(int valor01, int valor02)
+        {
+            var resultado = valor01 * valor02;
+
+            historico.Insert(0, $"{data} | {valor01} * {valor02} = {resultado}");
+
+            return resultado;
+        }
+
+        public List<string> Historico()
+        {
+            historico.RemoveRange(3, historico.Count - 3);
+            return historico;
+        }
     }
 }
--- Testes-xUnit/UnitTest1.cs
+++ Testes-xUnit/UnitTest1.cs
@@ -1,3 +1,4 @@
+using Caculadora;
 using System;
 using Xunit;
 
@@ -5,10 +6,95 @@ namespace Testes_xUnit
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        public Calculadora construirCalculadora()
         {
+            string data = "10/10/2000";
+            Calculadora calc = new Calculadora(data);
 
+            return calc;
         }
+
+        [Theory]
+        [InlineData (1, 2, 3)]
+        [InlineData (4, 5, 9)]
+        public void TestSomar(int valor1, int valor2, int resultado)
+        {
+            Calculadora calculadora = construirCalculadora();
+
+            int resultadoCalculadora = calculadora.Somar(valor1, valor2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(1, 2, 2)]
+        [InlineData(4, 5, 20)]
+        public void TestMultiplicar(int valor1, int valor2, int resultado)
+        {
+            Calculadora calculadora = construirCalculadora();
+
+            int resultadoCalculadora = calculadora.Multiplicar(valor1, valor2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(6, 2, 3)]
+        [InlineData(20, 4, 5)]
+        public void TestDividir(int valor1, int valor2, int resultado)
+        {
+            Calculadora calculadora = construirCalculadora();
+
+            int resultadoCalculadora = calculadora.Dividir(valor1, valor2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(10, 2, 8)]
+        [InlineData(4, 5, -1)]
+        public void TestSubitrair(int valor1, int valor2, int resultado)
+        {
+            Calculadora calculadora = construirCalculadora();
+
+            int resultadoCalculadora = calculadora.Subtrair(valor1, valor2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Fact]
+        public void TestDivisaoPorZero()
+        {
+            Calculadora calculadora = construirCalculadora();
+
+            Assert.Throws<DivideByZeroException>(
+                () => calculadora.Dividir(3, 0)
+            );
+        }
+
+        [Fact]
+        public void TestHistorico()
+        {
+            Calculadora calculadora = construirCalculadora();
+
+            calculadora.Somar(4, 5);
+            calculadora.Somar(4, 5);
+            calculadora.Somar(4, 5);
+            calculadora.Somar(4, 5);
+
+            var lista = calculadora.Historico();
+
+            Assert.NotEmpty(lista);
+            Assert.Equal(3, lista.Count);
+        }
+        //[Fact]
+        //public void Test2()
+        //{
+        //    Calculadora calculadora = new Calculadora();
+
+        //    int resultado = calculadora.Somar(4, 5);
+
+        //    Assert.Equal(9, resultado);
+        //}
     }
 }
