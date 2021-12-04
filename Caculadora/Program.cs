de79ff339a4633775b6458a117f60ac510713f9e
BrunoWM <bwillmoura3595@gmail.com>
2021-12-04 19:03:31 -0300

    Adicionar arquivos de projeto.

---
+++ Caculadora/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Caculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
